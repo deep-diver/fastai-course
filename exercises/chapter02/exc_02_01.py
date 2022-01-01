db59f9509130768f937dc10f83fbb92a005cb6bf
chansung park <chansung@chansungui-Macmini.local>
2022-01-01 23:49:15 +0900

    add section 1 of chap 2

--- exercises/chapter02/exc_02_01.py
+++ exercises/chapter02/exc_02_01.py
@@ -1,1 +1,9 @@
-공사중이지 말입니다.
+from fastai.vision.all import *
+
+url = "https://github.com/deep-diver/fastai-course/raw/master/data/oxford-iiit-pet-tiny.tar.gz"
+path = untar_data(url)/"images"
+files = get_image_files(path)
+
+dls = ImageDataLoaders.from_name_func(path, files, lambda f: f[0].isupper(), 
+                                      bs=4, item_tfms=Resize(224))
+dls.show_batch()
