9840463c76c5e2a989a44d110309a7ea3a35c837
deep-diver <deep.diver.csp@gmail.com>
2022-01-10 14:36:15 +0900

    add section 13 in chap 1

---
+++ exercises/chapter01/exc_01_13.py
@@ -0,0 +1,20 @@
+from fastcore.all import *
+
+@____________
+def happy_newyear(name:str): 
+  return f'happy new year, {name}!'
+
+@____________
+def happy_newyear(year:int): 
+  return f'happy new year, it\'s {year} now!'
+
+@____________
+def happy_newyear(name:str, year:int): 
+  return f'happy new year, {name}. It\'s {year} now!'
+
+print('등록된 함수 목록')
+print(happy_newyear)
+
+print(happy_newyear('chansung'))
+print(happy_newyear(2022))
+print(happy_newyear('chansung', 10))
---
+++ exercises/chapter01/test_01_13.py
@@ -0,0 +1,4 @@
+def test():
+    assert "@typedispatch" in __solution__, "@typedispatch를 사용하셨나요?"
+    
+    __msg__.good("잘 하셨습니다!")
